363fef199ec223d6c26b92f97fa49ce5b502a519
zager <1252496768@qq.com>
2023-12-19 14:04:18 +0800

    20231219

--- Public/debug_log/printf_log.c
+++ Public/debug_log/printf_log.c
@@ -1,12 +1,18 @@
 #include "printf_log.h"
-
 #ifdef ENABLE_DEBUG
-    #include "usart.h"
-    int _write(int file, char *data, int len)
-            {
-            HAL_StatusTypeDef status = HAL_UART_Transmit(&huart1, (uint8_t*)data, len, 1000);
-            return (status == HAL_OK ? len : 0);
-    }
+    #ifndef ENABLE_DEBUG_RTT
+        #include "usart.h"
+        int _write(int file, char *data, int len)
+                {
+                HAL_StatusTypeDef status = HAL_UART_Transmit(&huart1, (uint8_t*)data, len, 1000);
+                return (status == HAL_OK ? len : 0);
+        }
+    #else
+        void User_Printf_Init(void)
+        {
+            SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);            
+        }
+    #endif
 #endif
 
 
--- Public/debug_log/printf_log.h
+++ Public/debug_log/printf_log.h
@@ -1,22 +1,35 @@
 #ifndef __PRINTF_LOG__H
 #define __PRINTF_LOG__H
+
+#define DEBUG_INFO_NORMAL    (1)
+#define DEBUG_INFO_WARING    (2)
+#define DEBUG_INFO_ERR       (3)
+#define DEBUG_LEVEL          (DEBUG_INFO_NORMAL)
+
 #ifdef	ENABLE_DEBUG
     #ifdef ENABLE_DEBUG_RTT
-        #define USER_DEBUG(format,...)	SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+        #include "SEGGER_RTT.h"
+        #if(DEBUG_INFO_NORMAL >= DEBUG_LEVEL)
+            #define USER_DEBUG_NORMAL(format,...)	           SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+            #define USER_DEBUG_WARING(format,...)	           SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+            #define USER_DEBUG_ERR(format,...)	               SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+        #elif(DEBUG_INFO_WARING >= DEBUG_LEVEL)
+            #define USER_DEBUG_NORMAL(format,...)	
+            #define USER_DEBUG_WARING(format,...)	           SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+            #define USER_DEBUG_ERR(format,...)	               SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+        #elif(DEBUG_INFO_ERR >= DEBUG_LEVEL)
+            #define USER_DEBUG_NORMAL(format,...)	
+            #define USER_DEBUG_WARING(format,...)       
+            #define USER_DEBUG_ERR(format,...)	               SEGGER_RTT_printf(0,format,##__VA_ARGS__)
+        #else
+            #define USER_DEBUG_NORMAL(format,...)
+        #endif
+        void User_Printf_Init(void);
     #else
         #include "stdio.h"
-        #define USER_DEBUG(format,...)	printf(format,##__VA_ARGS__)
+        #define USER_DEBUG_NORMAL(format,...)	printf(format,##__VA_ARGS__)
     #endif
 #else
-	#define	USER_DEBUG(format,...)
+	#define	USER_DEBUG_NORMAL(format,...)
 #endif
-
-
-
-
-
-
-
-
-
 #endif
